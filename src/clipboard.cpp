ee5e4a747ea91c099ac227f79a19e2459f030f1d
Hermann Höhne <hoehermann@gmx.de>
2021-01-06 23:15:16 +0100

    More efficient copy to clipboard.

--- src/clipboard.cpp
+++ src/clipboard.cpp
@@ -4,29 +4,14 @@
 // based on https://forums.wxwidgets.org/viewtopic.php?t=19643
 void hpicviewFrame::CopyToClipboard() {
     if (!wxTheClipboard->IsOpened() && !wxTheClipboard->Open()) {
-        SetStatusText(
-            wxT("Could not access the clipboard."), 
-            STATUSBAR_COLUMN_MAIN
-        );
-        return;
+        throw std::runtime_error("Could not access the clipboard.");
     }
     if (m_image.IsOk()) {
         this->Enable(false);
-        SetStatusText(
-            wxT("Copying image to clipboard…"), 
-            STATUSBAR_COLUMN_MAIN
-        );
-        if (!wxTheClipboard->AddData(new wxBitmapDataObject(wxBitmap(m_image)))) {
-            SetStatusText(
-                wxT("Copying to clipboard has failed."), 
-                STATUSBAR_COLUMN_MAIN
-            );
-        } else {
-            SetStatusText(
-                wxT("Copying to clipboard has completed."), 
-                STATUSBAR_COLUMN_MAIN
-            );
-        }
+        bool ok = wxTheClipboard->AddData(new wxBitmapDataObject(bitmap->GetBitmap()));
         this->Enable(true);
+        if (!ok) {
+            throw std::runtime_error("Copying to clipboard has failed.");
+        }
     }
 }
